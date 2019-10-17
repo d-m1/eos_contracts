0db19aa8ae9e47623c184c93ba094f53dec9ba45
daniel.martin <daniel.martinarenas@soprasteria.com>
2019-10-17 15:06:35 +0200

    wip

--- user.cpp
+++ user.cpp
@@ -10,27 +10,27 @@ public:
   user(name receiver, name code,  datastream<const char*> ds): contract(receiver, code, ds) {}
 
   [[eosio::action]]
-  void append(name id, std::string role, time timestamp, std::vector<name> net_user, std::vector<name> net_subuser;) {
+  void append(name id, std::string role, time_t timestamp, std::vector<name> net_user, std::vector<name> net_subuser) {
     require_auth( id );
     user_index users( get_self(), get_first_receiver().value );
     auto iterator = users.find(id.value);
     if( iterator == users.end() )
     {
       users.emplace(id, [&]( auto& row ) {
-       row.id = id;
-       row.role = role;
-       row.timestamp = std::time(nullptr);
-       row.net_user = street;
-       row.net_subuser = city;
+        row.id = id;
+        row.role = role;
+        row.timestamp = timestamp;
+        row.net_user = net_user;
+        row.net_subuser = net_subuser;
       });
     }
     else {
       users.modify(iterator, user, [&]( auto& row ) {
         row.id = id;
         row.role = role;
-        row.timestamp = std::time(nullptr);
-        row.net_user = street;
-        row.net_subuser = city;
+        row.timestamp = timestamp;
+        row.net_user = net_user;
+        row.net_subuser = net_subuser;
       });
     }
   }
